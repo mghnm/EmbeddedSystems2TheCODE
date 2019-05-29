cde39ae346ee6cab27c1dcd6083a07ddb66f46c8
Drachenatan <49030306+Drachenatan@users.noreply.github.com>
2019-05-29 17:12:31 +0200

    Added Controller code

--- EmbeddedSystems2TheCODE/main.c
+++ EmbeddedSystems2TheCODE/main.c
@@ -36,6 +36,10 @@
 #define MPU_AYOFFSET 0.05
 #define MPU_AZOFFSET -0.2
 
+//Motorspeeds
+#define FULL_SPEED 127
+#define HALF_SPEED 63
+
 #define HIT_THRESHHOLD 0.18
 
 //Function prototypes for assisting subroutines.
@@ -59,332 +63,322 @@ void leftFwd(uint8_t spd);
 void leftRev(uint8_t spd);
 void rightStop();
 void leftStop();
+void turnRight(int speed);
+void turnLeft(int speed);
 void rightMotor(int speed);
 void leftMotor(int speed);
 void drive(int speed);
 void stop();
 
+volatile char run = '1';
 volatile char commCheck[3] = "No ";
+double raw;
+uint16_t numuS;
+uint8_t hit = 0;
 
 //Array of unsigned integers used to store up to 6 different sensor values located on different analog channels.
 volatile uint16_t sensorValues[6];
 
 //Initial channel set to Infra red sensor right.
-
 volatile uint8_t currentAnalogChannel = OPTICAL_FRONT;
-
-//volatile uint8_t currentAnalogChannel = OPTICAL_FRONT;
 
 //Interrupt service routine to update the sensor value array continuously using interrupts
 ISR(ADC_vect){
-	
-	
 	//There is a simple logic change that would allow up to 6 sensors 0-5 analog pins
-	
 	if(currentAnalogChannel > OPTICAL_RIGHT){
 		currentAnalogChannel = OPTICAL_FRONT;
-		} else {}
-		
-		
-		
-		selectChannel(currentAnalogChannel);
-		sensorValues[currentAnalogChannel] = ADCW;
-		currentAnalogChannel++;
-		startConversion();
-		
-		
 	}
 	
-	volatile char run = '1';
-	ISR(USART_RX_vect){
-		
-		switch(UDR0){
-			case 'A':
-			run = '1';
-			break;
-			case 'B':
-			/*leftMotor(0);
-			rightMotor(0);
-			_delay_ms(5000);*/
-			strcpy(commCheck,"Yes");
-			break;
-			case 'C':
-			leftMotor(0);
-			rightMotor(0);
+	selectChannel(currentAnalogChannel);
+	sensorValues[currentAnalogChannel] = ADCW;
+	currentAnalogChannel++;
+	startConversion();
+}
+
+ISR(USART_RX_vect) {
+	run = '1';
+	switch(UDR0) {
+		case 'H':
 			run = '0';
 			break;
-			default:
+		case 'L':
+			turnLeft(FULL_SPEED);
 			break;
-		}
-		
-		
+		case 'l':
+			turnLeft(HALF_SPEED);
+			break;
+		case 'R':
+			turnRight(FULL_SPEED);
+			break;
+		case 'r':
+			turnRight(HALF_SPEED);
+			break;
+		case 'F':
+			drive(FULL_SPEED);
+			break;
+		case 'f':
+			drive(HALF_SPEED);
+			break;
+		case 'B':
+			drive(-FULL_SPEED);
+			break;
+		case 'b':
+			drive(-HALF_SPEED);
+			break;
 	}
+}
+
+ISR(PCINT1_vect) {
+	if (bit_is_set(PINC,PORTC3)) {					// Checks if echo is high
+		TCNT1 = 0;								// Reset Timer
+		} else {
+		numuS = TCNT1;					// Save Timer value
+		uint8_t oldSREG = SREG;
+		cli();									// Disable Global interrupts
+		raw = numuS * 0.00107388316;
+
+		SREG = oldSREG;							// Enable interrupts
+		// Toggle debugging LED
+	}
+		
+}
+
+/* ****************************** Start of main *****************************/
+int main(void){
+	initUART0();
+	initializeMotors();
+	initializeADC();
+	initializeUltrasonic();
+	sei();
+	DDRB =	0xFF;
+		
+	char distance[100];
+	char axg_arr[10];
+	char ayg_arr[10];
+	char azg_arr[10];
+		
+	lcd_init(0xAF);    // init lcd and turn on
+	//init mpu6050
+	mpu6050_init();
+	_delay_ms(50);
+		
+	#if MPU6050_GETATTITUDE == 0
+	int16_t ax = 0;
+	int16_t ay = 0;
+	int16_t az = 0;
+	int16_t gx = 0;
+	int16_t gy = 0;
+	int16_t gz = 0;
+	double axg = 0;
+	double ayg = 0;
+	double azg = 0;
+	double gxds = 0;
+	double gyds = 0;
+	double gzds = 0;
+	#endif
+
+	while(1) {
+		_delay_ms(60);
+		
+		#if MPU6050_GETATTITUDE == 0
+		mpu6050_getConvAccelData(&axg, &ayg, &azg);
+		axg = axg + MPU_AXOFFSET;
+		ayg = ayg + MPU_AYOFFSET;
+		azg = azg + MPU_AZOFFSET;
+		
+		if((axg > HIT_THRESHHOLD || axg < -HIT_THRESHHOLD) || (ayg > HIT_THRESHHOLD || ayg < -HIT_THRESHHOLD)){
+			hit = 1;
+		}
+		#endif
+		
+		sendUltraSonicSignal();
+		dtostrf(raw, 3, 1, distance);
+		
+		lcd_gotoxy(0,0);
+		lcd_puts("Distance: ");
+		lcd_puts(distance);
+		lcd_puts("   ");
+		//lcd_display();
+		
+		lcd_gotoxy(12,3);
+		lcd_puts("Comm: ");
+		lcd_puts(commCheck);
+		lcd_puts("   ");
+		//lcd_display();
+		
+		lcd_gotoxy(0,5);
+		lcd_puts("Status: ");
+		lcd_puts(hit);
+		lcd_puts("   ");
+		lcd_display();
+			
+		if(raw > 10 && !hit) {
+			drive(126);
+		} else {
+			drive(0);
+		}
+	}
+	return 0;
+}
+
+/* ****************************** End of Main *****************************/
+
+//Changes the contents of the relevant registers to allow for PWM on 2 separate pins that serve as input for the motor-driver module.
+void initializePWM(){
+		
+	DDRD |= (1 << PWM_L) | (1 << PWM_R);
+		
+	OCR0A = 0;
+	OCR0B = 0;
+
+	// set none-inverting mode
+	TCCR0A |= (1 << COM0A1);
+	TCCR0A |= (1 << COM0B1);
+
+	// set fast PWM Mode
+	TCCR0A |= (1 << WGM01) | (1 << WGM00);
+		
+		
+	// set prescaler to 8 and starts PWM
+	//Temp disabled prescaler
+	TCCR0B |= (1 << CS02) | (1 << CS00);
+}
+
+//initializeMotors is a convenient subroutine which can be called in order set the required pins to output and initializes PWM with the expectation
+//that the motors should not work if PWM has not been initialized.
+void initializeMotors(){
+	initializePWM();
+		
+	DDRD |= (1 << L_CTRL_1) | (1 << L_CTRL_2) | (1 << R_CTRL_1);
+	DDRB |= (1 << R_CTRL_2);
+		
+}
 	
-	double raw;
-	uint16_t numuS;
-	uint8_t hit = 0;
+//Initialization of pin mask interrupt for the echo to get accurate readings
+void initializeUltrasonic(){
+		
+	DDRC = 0xFF;							// Port C all output. PC0: RW		PC1: RS		PC2: E
+	DDRC &= ~(1<<DDC3);						// Set Pin C5 as input to read Echo
+	PORTC |= (1<<PORTC3);					// Enable pull up on C5
+	PORTC &= ~(1<<PORTC2);						// Init C4 as low (trigger)
+
+	PRR &= ~(1<<PRTIM1);					// To activate timer1 module
+	TCNT1 = 0;								// Initial timer value
+	TCCR1B |= (1<<CS10);					// Timer without prescaller. Since default clock for atmega328p is 1Mhz period is 1uS
+	TCCR1B |= (1<<ICES1);					// First capture on rising edge
+
+	PCICR = (1<<PCIE1);						// Enable PCINT[14:8] we use pin C5 which is PCINT13
+	PCMSK1 = (1<<PCINT11);					// Enable C5 interrupt
+}
+
+//Rotate the leftMotor CW
+void leftFwd(uint8_t spd){
+	PORTD |= (1 << L_CTRL_1);
+	PORTD &= ~(1 << L_CTRL_2);
+	analogWrite(PWM_L, spd);
+		
+}
+
+//Rotate the leftMotor CCW
+void leftRev(uint8_t spd){
+	PORTD &= ~(1 << L_CTRL_1);
+	PORTD |= (1 << L_CTRL_2);
+	analogWrite(PWM_L, spd);
+}
+
+//Rotate right motor CW
+void rightFwd(uint8_t spd){
+	PORTD |= (1 << R_CTRL_1);
+	PORTB &= ~(1 << R_CTRL_2);
+	analogWrite(PWM_R, spd);
+}
+
+//Rotate right motor CCW
+void rightRev(uint8_t spd){
+	PORTD &= ~(1 << R_CTRL_1);
+	PORTB |= (1 << R_CTRL_2);
+	analogWrite(PWM_R, spd);
+}
+
+//Stop left motor
+void leftStop(){
+	PORTD &= ~(1 << L_CTRL_1);
+	PORTD &= ~(1 << L_CTRL_2);
+	analogWrite(PWM_L, 0);
+}
+
+//Stop right motor
+void rightStop(){
+	PORTD &= ~(1 << R_CTRL_1);
+	PORTB &= ~(1 << R_CTRL_2);
+	analogWrite(PWM_R, 0);
+}
+
+//Stop both motors
+void stop(){
+	leftStop();
+	rightStop();
+}
+
+//Takes in a value either positive or negative that will drive both motors at the same speed. If the value is negative the car will move CCW or in reverse
+void drive(int speed) {
+	if(speed > 0){
+		leftFwd((uint8_t) abs(speed));
+		rightRev((uint8_t) abs(speed));
+	} else {
+		leftRev((uint8_t) abs(speed));
+		rightFwd((uint8_t) abs(speed));
+	}
+}
+
+void turnRight(int speed) {
+	rightMotor(speed/2);
+	leftMotor(speed);
+}
+
+void turnLeft(int speed) {
+	rightMotor(speed);
+	leftMotor(speed/2);
+}
+
+void rightMotor(int speed) {
+	if(speed > 0) {
+		rightFwd((uint8_t) abs(speed));
+	} else {
+		rightRev((uint8_t) abs(speed));
+	}
+}
+
+void leftMotor(int speed) {
+	if(speed > 0){
+		//leftFwd((uint8_t) abs(speed));
+		leftRev((uint8_t) abs(speed));
+		} else {
+		//leftRev((uint8_t) abs(speed));
+		leftFwd((uint8_t) abs(speed));
+	}
+}
 	
-	
-	ISR(PCINT1_vect) {
-		if (bit_is_set(PINC,PORTC3)) {					// Checks if echo is high
-			TCNT1 = 0;								// Reset Timer
-			} else {
-			numuS = TCNT1;					// Save Timer value
-			uint8_t oldSREG = SREG;
-			cli();									// Disable Global interrupts
-			raw = numuS * 0.00107388316;
-
-			SREG = oldSREG;							// Enable interrupts
-			// Toggle debugging LED
-		}
-		
-	}
-
-
-	/* ****************************** Start of main *****************************/
-
-	int main(void){
-		
-		initUART0();
-		initializeMotors();
-		initializeADC();
-		initializeUltrasonic();
-		sei();
-		DDRB =	0xFF;
-		
-		char distance[100];
-		char axg_arr[10];
-		char ayg_arr[10];
-		char azg_arr[10];
-		
-		lcd_init(0xAF);    // init lcd and turn on
-		//init mpu6050
-		mpu6050_init();
-		_delay_ms(50);
-		
-			#if MPU6050_GETATTITUDE == 0
-			int16_t ax = 0;
-			int16_t ay = 0;
-			int16_t az = 0;
-			int16_t gx = 0;
-			int16_t gy = 0;
-			int16_t gz = 0;
-			double axg = 0;
-			double ayg = 0;
-			double azg = 0;
-			double gxds = 0;
-			double gyds = 0;
-			double gzds = 0;
-			#endif
-
-		while(1){
-			_delay_ms(60);
-			
-			
-			#if MPU6050_GETATTITUDE == 0
-			mpu6050_getConvAccelData(&axg, &ayg, &azg);
-			axg = axg + MPU_AXOFFSET;
-			ayg = ayg + MPU_AYOFFSET;
-			azg = azg + MPU_AZOFFSET;
-			
-			#endif
-			
-			#if MPU6050_GETATTITUDE == 0
-			
-			if((axg > HIT_THRESHHOLD || axg < -HIT_THRESHHOLD) || (ayg > HIT_THRESHHOLD || ayg < -HIT_THRESHHOLD)){
-				hit = 1;
-			}
-			#endif
-			
-			sendUltraSonicSignal();
-			dtostrf(raw, 3, 1, distance);
-			
-			
-			lcd_gotoxy(0,0);
-			lcd_puts("Distance: ");
-			lcd_puts(distance);
-			lcd_puts("   ");
-			//lcd_display();
-			
-			lcd_gotoxy(12,3);
-			lcd_puts("Comm: ");
-			lcd_puts(commCheck);
-			lcd_puts("   ");
-			//lcd_display();
-					
-			
-		    lcd_gotoxy(0,5);
-			lcd_puts("Status: ");
-			lcd_puts(hit);
-			lcd_puts("   ");
-			lcd_display();
-			
-			
-			
-			
-			if(raw > 10 && !hit){
-				drive(126);
-			} else {
-				drive(0);
-			}
-			
-		}
-		
-		return 0;
-	}
-
-
-	/* ****************************** End of Main *****************************/
-
-
-
-	//Changes the contents of the relevant registers to allow for PWM on 2 separate pins that serve as input for the motor-driver module.
-	void initializePWM(){
-		
-		DDRD |= (1 << PWM_L) | (1 << PWM_R);
-		
-		OCR0A = 0;
-		OCR0B = 0;
-
-		// set none-inverting mode
-		TCCR0A |= (1 << COM0A1);
-		TCCR0A |= (1 << COM0B1);
-
-		// set fast PWM Mode
-		TCCR0A |= (1 << WGM01) | (1 << WGM00);
-		
-		
-		// set prescaler to 8 and starts PWM
-		//Temp disabled prescaler
-		TCCR0B |= (1 << CS02) | (1 << CS00);
-	}
-
-	//initializeMotors is a convenient subroutine which can be called in order set the required pins to output and initializes PWM with the expectation
-	//that the motors should not work if PWM has not been initialized.
-	void initializeMotors(){
-		initializePWM();
-		
-		DDRD |= (1 << L_CTRL_1) | (1 << L_CTRL_2) | (1 << R_CTRL_1);
-		DDRB |= (1 << R_CTRL_2);
-		
-	}
-	
-	//Initialization of pin mask interrupt for the echo to get accurate readings
-	void initializeUltrasonic(){
-		
-		DDRC = 0xFF;							// Port C all output. PC0: RW		PC1: RS		PC2: E
-		DDRC &= ~(1<<DDC3);						// Set Pin C5 as input to read Echo
-		PORTC |= (1<<PORTC3);					// Enable pull up on C5
-		PORTC &= ~(1<<PORTC2);						// Init C4 as low (trigger)
-
-		PRR &= ~(1<<PRTIM1);					// To activate timer1 module
-		TCNT1 = 0;								// Initial timer value
-		TCCR1B |= (1<<CS10);					// Timer without prescaller. Since default clock for atmega328p is 1Mhz period is 1uS
-		TCCR1B |= (1<<ICES1);					// First capture on rising edge
-
-		PCICR = (1<<PCIE1);						// Enable PCINT[14:8] we use pin C5 which is PCINT13
-		PCMSK1 = (1<<PCINT11);					// Enable C5 interrupt
-	}
-
-	//Rotate the leftMotor CW
-	void leftFwd(uint8_t spd){
-		PORTD |= (1 << L_CTRL_1);
-		PORTD &= ~(1 << L_CTRL_2);
-		analogWrite(PWM_L, spd);
-		
-	}
-
-	//Rotate the leftMotor CCW
-	void leftRev(uint8_t spd){
-		PORTD &= ~(1 << L_CTRL_1);
-		PORTD |= (1 << L_CTRL_2);
-		analogWrite(PWM_L, spd);
-	}
-
-	//Rotate right motor CW
-	void rightFwd(uint8_t spd){
-		PORTD |= (1 << R_CTRL_1);
-		PORTB &= ~(1 << R_CTRL_2);
-		analogWrite(PWM_R, spd);
-	}
-
-	//Rotate right motor CCW
-	void rightRev(uint8_t spd){
-		PORTD &= ~(1 << R_CTRL_1);
-		PORTB |= (1 << R_CTRL_2);
-		analogWrite(PWM_R, spd);
-	}
-
-	//Stop left motor
-	void leftStop(){
-		PORTD &= ~(1 << L_CTRL_1);
-		PORTD &= ~(1 << L_CTRL_2);
-		analogWrite(PWM_L, 0);
-	}
-
-	//Stop right motor
-	void rightStop(){
-		PORTD &= ~(1 << R_CTRL_1);
-		PORTB &= ~(1 << R_CTRL_2);
-		analogWrite(PWM_R, 0);
-	}
-
-	//Stop both motors
-	void stop(){
-		leftStop();
-		rightStop();
-	}
-
-	//Takes in a value either positive or negative that will drive both motors at the same speed. If the value is negative the car will move CCW or in reverse
-	void drive(int speed){
-		if(speed > 0){
-			leftFwd((uint8_t) abs(speed));
-			rightRev((uint8_t) abs(speed));
-			} else {
-			leftRev((uint8_t) abs(speed));
-			rightFwd((uint8_t) abs(speed));
-		}
-	}
-
-	void rightMotor(int speed){
-		if(speed > 0){
-			rightFwd((uint8_t) abs(speed));
-			} else {
-			rightRev((uint8_t) abs(speed));
-		}
-	}
-
-	void leftMotor(int speed){
-		if(speed > 0){
-			//leftFwd((uint8_t) abs(speed));
-			leftRev((uint8_t) abs(speed));
-			} else {
-			//leftRev((uint8_t) abs(speed));
-			leftFwd((uint8_t) abs(speed));
-		}
-	}
-	
-	void sendUltraSonicSignal(){
-		PORTC |= (1<<PORTC2);						// Set trigger high
-		_delay_us(10);							// for 10uS
-		PORTC &= ~(1<<PORTC2);						// to trigger the ultrasonic module
-	}
+void sendUltraSonicSignal(){
+	PORTC |= (1<<PORTC2);						// Set trigger high
+	_delay_us(10);							// for 10uS
+	PORTC &= ~(1<<PORTC2);						// to trigger the ultrasonic module
+}
 
 	
-	//analogWrite works in conjunction with initializePWM where we enable timer0, OCR0B and OCR0A. YOU MUST INITIALIZE PWM BEFORE ANALOGWRITE WOULD WORK.
-	//This sub-routine defines the dutyCycle of the motors by writing to the respective timer0 registers which will drive the motors.
-	void analogWrite(uint8_t pin, uint8_t dutyCycle){
-		
-		//TODO: Do some checking so that value is between 0-255
-		
-		if(pin == PWM_L){
-			OCR0B = dutyCycle;
-			}else if(pin == PWM_R){
-			OCR0A = dutyCycle;
-			}else{
-			//Do something weird
-		}
+//analogWrite works in conjunction with initializePWM where we enable timer0, OCR0B and OCR0A. YOU MUST INITIALIZE PWM BEFORE ANALOGWRITE WOULD WORK.
+//This sub-routine defines the dutyCycle of the motors by writing to the respective timer0 registers which will drive the motors.
+void analogWrite(uint8_t pin, uint8_t dutyCycle) {
+	
+	//TODO: Do some checking so that value is between 0-255
+	if(pin == PWM_L) {
+		OCR0B = dutyCycle;
+	} else if(pin == PWM_R) {
+		OCR0A = dutyCycle;
+	} else {
+		//Do something weird
 	}
+}
 
 	//Initializes the ADC registers to support the IR and optical sensors.
 	void initializeADC(){
--- EmbeddedSystems2TheCODE/uart.h
+++ EmbeddedSystems2TheCODE/uart.h
@@ -2,9 +2,9 @@
 #define _UART_H_
 
 #include <avr/io.h> 
-#include <util/delay.h> 
+/*#include <util/delay.h> 
 #include <stdio.h> 
-#include <stdlib.h> 
+#include <stdlib.h> */
 
 
 
@@ -28,8 +28,8 @@
  *********************************************************************/
 void initUART0()
 {	
-	UBRR0H = (UART_BAUD_SELECT0>>8); 
-  	UBRR0L = UART_BAUD_SELECT0; 
+	UBRR0H = (UART_BAUD_SELECT0>>8);
+  	UBRR0L = UART_BAUD_SELECT0;
 	UCSR0A = 0;
 	// No double speed      see ATMEGA168A datasheet page 185
 	UCSR0B = (1<<RXCIE0) | (1<<RXEN0) | (1<<TXEN0);	//Enable receiver, receiver interrupt, transmitter
@@ -37,45 +37,17 @@ void initUART0()
 	UCSR0A |= (1<<TXC0);							// clear any existing transmits	
 }
 
-
-
-/******************************************************************//**
- * Sends a single character via UART1 (eg. to a Hyperterminal).
- *********************************************************************/
-/*void initUART1()
-{
-	UBRR1H = (UART_BAUD_SELECT1>>8); 
-  	UBRR1L = UART_BAUD_SELECT1; 
-	UCSR1A = 0;  
-	// No double speed
-	UCSR1B = (1<<RXCIE1) | (1<<RXEN1) | (1<<TXEN1);		// Enable receiver, receiver interrupt, transmitter
-	UCSR1C = (1<<UCSZ11) | (1<<UCSZ10) ;				// Set frame format: 8data,  1 stop bit,  no parity
-	UCSR1A |= (1<<TXC1);								// clear any existing transmits	
-}*/
-
 /******************************************************************//**
  * Sends a single character via UART0 (eg. to a Hyperterminal).
  *********************************************************************/
 void uart0_putc(unsigned char c)   //USART_Receive0
 {
     while (!(UCSR0A & (1<<UDRE0)))  // wait until sending is possible
-    {
-    }                             
+    {;}
     UDR0 = c;                       // sending signs
 }
 
-/******************************************************************//**
- * Sends a single character via UART1 (eg. to a Hyperterminal).
- *********************************************************************/
-/*void uart1_putc(unsigned char c)   //USART_Receive1
-{
-    while (!(UCSR1A & (1<<UDRE1)))  // wait until sending is possible
-    {
-    }                             
-    UDR1 = c;                       // sending signs
-}*/
 
- 
 /******************************************************************//**
  * Sends a complete string via UART (eg. to a Hyperterminal).
  *********************************************************************/
@@ -88,17 +60,7 @@ void uart0_puts (char *s)
     }
 }
 
-/******************************************************************//**
- * Sends a complete string via UART (eg. to a Hyperterminal).
- *********************************************************************/
-/*void uart1_puts (char *s)
-{
-    while (*s)
-    {                               // sending char until "/0"
-        uart1_putc(*s);
-        s++;
-    }
-}*/
+
 /******************************************************************//**
  * Sends an Integer via UART (eg. to a Hyperterminal).
  *********************************************************************/
